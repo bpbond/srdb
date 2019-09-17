9df9bbab4f2431338fc24d7450956097ddedd02f
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2019-09-17 04:48:18 -0400

    Add comments

--- R/srdb_check.R
+++ R/srdb_check.R
@@ -3,6 +3,10 @@
 
 srdb <- read.csv("srdb-data.csv", stringsAsFactors = FALSE)
 
+# -------------------------------------------------------------------------
+# The `Record_number` field is crucial, linking a variety of tables and
+# providing reproducibility between versions.
+ 
 rn_nas <- which(is.na(srdb$Record_number))
 if(length(rn_nas)) {
   warning("There are ", length(rn_nas), " empty (NA) Record_number fields")
@@ -13,6 +17,7 @@ if(length(rn_dupes)) {
   warning("There are ", length(rn_dupes), " duplicated Record_number fields")
 }
 
+# Commented out for now--not sure if duplicated rows are allowed
 #dupes <- which(duplicated(srdb[-1]))
 #if(length(dupes)) {
 #  warning("There are ", length(dupes), " duplicated rows")
